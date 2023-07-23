559d13f0f4416b7bdacd8eeaf4d3dcf9a52b5681
ken <wailinhtetaung007@gmail.com>
2023-07-24 00:17:19 +0630

    cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://mha.itgatewaymm.com/'],
 
     'allowed_origins_patterns' => [],
 
